60a4b1089873239dae239c910ab67097b6c88ea4
YSJ <shaojieyew@gmail.com>
2020-09-10 01:08:25 +0800

    first commit

---
+++ src/main/scala/example/SummaryExporter.scala
@@ -0,0 +1,52 @@
+package example
+
+import io.prometheus.client.Summary
+import io.prometheus.client.exporter.MetricsServlet
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.{ ServletContextHandler, ServletHolder }
+
+object SummaryExporter {
+  val receivedBytes = Summary
+    .build()
+    .name("requests_size_bytes")
+    .help("Request size in bytes.")
+    .register();
+  val requestLatency = Summary
+    .build()
+    .name("requests_latency_seconds")
+    .help("Request latency in seconds.")
+    .labelNames("time")
+    .register();
+  val requestLatency2 = Summary
+    .build()
+    .quantile(0.5, 0.05) // Add 50th percentile (= median) with 5% tolerated error
+    .quantile(0.9, 0.01) // Add 90th percentile with 1% tolerated error
+    .name("requests_latency_seconds_50_90_percentil")
+    .help("Request latency in seconds.")
+    .register();
+
+  def main(args: Array[String]): Unit = {
+    new Thread(new Runnable {
+      override def run(): Unit = {
+        val server  = new Server(1234)
+        val context = new ServletContextHandler()
+        context.setContextPath("/")
+        server.setHandler(context)
+        context.addServlet(new ServletHolder(new MetricsServlet()), "/metrics")
+        server.start()
+        server.join()
+      }
+    }).start()
+
+    while (true) {
+      val requestTimer  = requestLatency.labels((System.currentTimeMillis() / 1000).toString).startTimer();
+      val requestTimer2 = requestLatency2.startTimer();
+
+      receivedBytes.observe(Math.random() * 10);
+      Thread.sleep(1000)
+      requestTimer.observeDuration();
+      requestTimer2.observeDuration();
+      println(System.currentTimeMillis())
+    }
+  }
+}
---
+++ src/main/scala/exporter/YarnStatusExporter.scala
@@ -0,0 +1,84 @@
+package exporter
+
+import java.util
+
+import http.KerberosHttpClient.request
+import org.apache.commons.io.IOUtils
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClientBuilder
+import io.circe.{ parser, Json }
+import io.prometheus.client.{ Counter, Gauge }
+import io.prometheus.client.exporter.MetricsServlet
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.{ ServletContextHandler, ServletHolder }
+
+object YarnStatusExporter {
+  val gauge = Gauge
+    .build()
+    .name("spark_app_state")
+    .help("Number of spark app running by application name")
+    .labelNames(Array("app_name"): _*)
+    .register();
+  /*
+  http://localhost:8088/ws/v1/cluster/apps?states=running,accepted&applicationTypes=Spark
+  http://localhost:8088/ws/v1/cluster/appstatistics?states=running&applicationTypes=Spark
+  http://localhost:8088/ws/v1/cluster/nodes/
+   */
+  def main(args: Array[String]): Unit = {
+
+    val kerberosEnable = false
+    val userid         = "HTTP/test@test.com"
+    val principle      = "HTTP/test@test.com"
+    val keytabLocation = "file://C://Development//test.keytab"
+    val url            = "http://localhost:8088/ws/v1/cluster/apps?states=running,accepted&applicationTypes=Spark"
+
+    new Thread(new Runnable {
+      override def run(): Unit = {
+        val server  = new Server(1234)
+        val context = new ServletContextHandler()
+        context.setContextPath("/")
+        server.setHandler(context)
+        context.addServlet(new ServletHolder(new MetricsServlet()), "/metrics")
+        server.start()
+        server.join()
+      }
+    }).start()
+
+    while (true) {
+
+      val httpGetRequest = new HttpGet(url)
+      val response = if (kerberosEnable) {
+        request(new HttpGet(url), userid, principle, keytabLocation)
+      } else {
+        HttpClientBuilder.create.build().execute(httpGetRequest)
+      }
+
+      val inputStream = response.getEntity.getContent
+      val result      = new String(IOUtils.toByteArray(inputStream), "UTF-8")
+
+      val json: Json        = parser.parse(result).right.getOrElse(Json.Null)
+      var app: Option[Json] = None
+      var index: Int        = 0
+
+      var runningList = Map[String, Int]()
+
+      do {
+        val jsonApp = json.hcursor.downField("apps").downField("app").downN(index)
+        app = jsonApp.focus
+
+        if (app.isDefined) {
+          val name = jsonApp.get[String]("name").getOrElse("")
+          if (name.length > 0) {
+            runningList += (name -> (runningList.get(name).getOrElse(0) + 1))
+          }
+        }
+        index += 1
+      } while (app.isDefined)
+
+      runningList.map(app => {
+        gauge.labels(app._1).set(app._2)
+      })
+      Thread.sleep(1000)
+    }
+  }
+}
